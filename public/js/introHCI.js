fd28e47a2736ecfec6b3de558a62adf0ff361039
Justin Park <jep067@ucsd.edu>
2018-02-22 11:00:30 -0800

    lab7

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -9,5 +9,9 @@ $(document).ready(function() {
  * Function that is called when the document is ready.
  */
 function initializePage() {
-	// your code here
+	console.log("Init");
+	$(".likeBtn").click(function(e) {
+		console.log("CLICK");
+		ga('send', 'event', 'like', 'click');
+	});
 }
